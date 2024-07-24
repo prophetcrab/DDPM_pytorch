bef818388cb8c3eeb5b2d38b2cac95fcb47ef060
prophetcrab <35569881+prophetcrab@users.noreply.github.com>
2024-07-25 05:46:07 +0900

    Add files via upload

---
+++ Diffusion2D/predict.py
@@ -0,0 +1,20 @@
+# -------------------------------------#
+#   运行predict.py可以生成图片
+#   生成1x1的图片和5x5的图片
+# -------------------------------------#
+from DDPM.ddpm import Diffusion
+
+if __name__ == "__main__":
+    save_path_5x5 = r"D:\PythonProject2\DiffusionModel\Diffusion2D\results\predict_out\predict_1x1_results.png"
+    save_path_1x1 = r"D:\PythonProject2\DiffusionModel\Diffusion2D\results\predict_out\predict_5x5_results.png"
+
+    ddpm = Diffusion()
+    while True:
+        img = input('Just Click Enter~')
+        print("Generate_1x1_image")
+        ddpm.generate_1x1_image(save_path_1x1)
+        print("Generate_1x1_image Done")
+
+        # print("Generate_5x5_image")
+        # ddpm.generate_5x5_image(save_path_5x5)
+        # print("Generate_5x5_image Done")
